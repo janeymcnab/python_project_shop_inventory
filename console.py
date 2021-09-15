70a632b5ef7597cc7fe6c43ade85e54603be46d5
Jane McNab <writetojaney@hotmail.co.uk>
2021-09-15 15:52:48 +0100

    adds repositories and console file

--- console.py
+++ console.py
@@ -1,3 +1,12 @@
 import pdb 
 from models.product import Product
 from models.manufacturer import Manufacturer
+
+import repositories.manufacturer_repository as manufacturer_repository
+import repositories.product_repository as product_repository
+
+# product_repository.delete_all()
+# manufacturer_repository.delete_all()
+
+
+pdb.set_trace()
