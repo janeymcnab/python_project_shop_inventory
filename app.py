4f1aac273c4fe59d6e6d30b4cb9aea73fa20eaad
Jane McNab <writetojaney@hotmail.co.uk>
2021-09-15 17:36:15 +0100

    adds flask and creates app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Controllers go here -
+# from controllers.name_controller import names_blueprint
+
+app = Flask(__name__)
+
+# app goes here -
+# app.register(names_blueprint)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
