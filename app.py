8f70fd3773c3a57caa54fa2908aeedef2aee9588
Jane McNab <writetojaney@hotmail.co.uk>
2021-09-16 11:46:19 +0100

    completes app.py

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, render_template
 
 # Controllers go here -
 # from controllers.name_controller import names_blueprint
+from controllers.manufacturers_controller import manufacturers_blueprint
+from controllers.products_controller import products_blueprint
 
 app = Flask(__name__)
 
 # app goes here -
 # app.register(names_blueprint)
+app.register_blueprint(manufacturers_blueprint)
+app.register_blueprint(products_blueprint)
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
